f2b81565cd2d1ab2a4a7d29d7a8023a31ddb25f0
Questionable Existence <101468308+ScripterRickster@users.noreply.github.com>
2024-01-06 17:34:59 -0500

    Create modlogModel.js

---
+++ modlogModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model(
+
+  "modlogs",
+  new mongoose.Schema({
+    userId: String,
+    type: String,
+    guildId: String,
+    moderatorId: String,
+    reason: String,
+    timestamp: String,
+    duration: String,
+  })
+  
+)
+  
